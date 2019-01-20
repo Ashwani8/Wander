8c40c6399507b0b3a5896ecb948cb88471ac71f6
Ashwani Kumar <akumar@monmouthcollege.edu>
2019-01-20 13:29:48 -0600

    refactored java class name

--- app/src/main/java/com/example/admin/wander/Attraction.java
+++ app/src/main/java/com/example/admin/wander/Attraction.java
@@ -1,13 +1,13 @@
 package com.example.admin.wander;
 
-public class attraction {
+public class Attraction {
     // step 1 create variables
     private int image; // because i am going to create a static recycler, i.e.
     // we will use images from drawable and not from network
     private String title, shortDescription;
     // step 2 define the variable and then create a constructor for all just by
     // right clicking >generate>constructor select all the value
-    public attraction(int image, String title, String shortDescription) {
+    public Attraction(int image, String title, String shortDescription) {
         this.image = image;
         this.title = title;
         this.shortDescription = shortDescription;
---
+++ app/src/main/java/com/example/admin/wander/AttractionAdapter.java
@@ -0,0 +1,8 @@
+package com.example.admin.wander;
+/*
+ * we will need two classes one for RecyclerView.Adapter
+ * and one to hold view for RecyclerView.ViewHolder
+ */
+public class AttractionAdapter {
+
+}
